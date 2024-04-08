1f29174b20a51b13f5a3a5eee58dd8e86f2129af
Etrama22 <muhazis73@gmail.com>
2024-04-08 14:22:07 +0700

    Update the app to display the image section

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
             ),
             body: const SingleChildScrollView(
               child: Column(children: [
+                ImageSection(
+                  image: 'images/lake.jpg',
+                ),
                 TitleSection(
                     name: 'Oeschinen Lake Campground',
                     location: 'Kandersteg, Switzerland'),
