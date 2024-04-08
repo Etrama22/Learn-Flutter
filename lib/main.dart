0ff9dd6ae65b30e1ed2a6776be6d96a3011100cf
Etrama22 <muhazis73@gmail.com>
2024-04-08 13:37:49 +0700

    Update the app to display the text section

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,16 @@ class MyApp extends StatelessWidget {
                     name: 'Oeschinen Lake Campground',
                     location: 'Kandersteg, Switzerland'),
                 ButtonSection(),
+                TextSection(
+                  description:
+                      'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                      'Bernese Alps. Situated 1,578 meters above sea level, it '
+                      'is one of the larger Alpine Lakes. A gondola ride from '
+                      'Kandersteg, followed by a half-hour walk through pastures '
+                      'and pine forest, leads you to the lake, which warms to 20 '
+                      'degrees Celsius in the summer. Activities enjoyed here '
+                      'include rowing, and riding the summer toboggan run.',
+                )
               ]),
             )));
   }
