bafb229b55e6e3a0ac19cebbd0106ffb4d66329e
Etrama22 <muhazis73@gmail.com>
2024-04-08 13:18:36 +0700

    Change the app body to a scrolling view

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
     return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        title: appTitle,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text(appTitle),
+            ),
+            body: const SingleChildScrollView(
+              child: Column(children: []),
+            )));
   }
 }
 
