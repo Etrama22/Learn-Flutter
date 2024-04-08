e5c902a7abca05ebcf4b65b9e986b7989a12335a
Etrama22 <muhazis73@gmail.com>
2024-04-08 13:36:23 +0700

    Add the TextSection widget

--- lib/main.dart
+++ lib/main.dart
@@ -108,6 +108,26 @@ class ButtonSection extends StatelessWidget {
   }
 }
 
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+  }
+}
+
 class ButtonWithText extends StatelessWidget {
   const ButtonWithText({
     super.key,
